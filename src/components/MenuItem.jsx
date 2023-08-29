4870a3c7d3dfb92e3b0d99e1d90fa448e7fc7144
stormymayday <stormymayday7@gmail.com>
2023-08-28 20:12:32 -0700

    Implemented MenuItem.jsx

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,6 +1,15 @@
-const MenuItem = () => {
+const MenuItem = ({ title, category, price, img, desc }) => {
     return (
-        <div>MenuItem</div>
+        <article className="menu-item">
+            <img className="img" src={img} alt={title} />
+            <div className="item-info">
+                <header>
+                    <h5>{title}</h5>
+                    <span className="item-price">{price}</span>
+                </header>
+                <p className="item-text">{desc}</p>
+            </div>
+        </article>
     );
 }
 

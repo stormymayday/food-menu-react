6d717dc0e521314614b4d1b5c08e290bb77a37b4
stormymayday <stormymayday7@gmail.com>
2023-08-28 17:27:05 -0700

    Importing data. Setting up menu using useState.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Title from "./components/Title.jsx";
+import data from './data.js';
 
 const App = () => {
+
+  const [menu, setMenu] = useState(data);
+
   return (
     <main>
       <section className="menu">

6a60f25bf7609043e424ff9c8755a9d15167e30c
stormymayday <stormymayday7@gmail.com>
2024-11-24 22:13:31 -0800

    Category Filter

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,35 @@ import { MenuItemType } from "@/types";
 import { useState } from "react";
 import Title from "@/components/Title";
 import Menu from "@/components/Menu";
-import Categories from "@/components/Categories";
+import CategoryFilter from "@/components/CategoryFilter";
 
 const uniqueCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
 export default function Home() {
-    console.log(uniqueCategories);
+    const [menuItems, setMenuItems] = useState<MenuItemType[]>(menu);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [menuItems, setMenuItems] = useState<MenuItemType[]>(menu);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [categories, setCategories] = useState(uniqueCategories);
+
+    const filterMenuItems = (category: string, menu: MenuItemType[]) => {
+        if (category === "all") {
+            setMenuItems(menu);
+            return;
+        }
+        const newItems = menu.filter((menuItem) => {
+            return menuItem.category === category;
+        });
+        setMenuItems(newItems);
+    };
+
     return (
         <main>
             <section className="menu">
                 <Title text="our menu" />
-                <Categories categories={categories} />
+                <CategoryFilter
+                    categories={categories}
+                    filterMenuItems={filterMenuItems}
+                />
                 <Menu items={menuItems} />
             </section>
         </main>
---
+++ components/CategoryFilter.tsx
@@ -0,0 +1,28 @@
+import { MenuItemType } from "@/types";
+import menu from "@/data";
+
+interface CategoriesProps {
+    categories: string[];
+    filterMenuItems: (category: string, menu: MenuItemType[]) => void;
+}
+
+function CategoryFilter({ categories, filterMenuItems }: CategoriesProps) {
+    return (
+        <div className="btn-container">
+            {categories.length > 0 &&
+                categories.map((category) => {
+                    return (
+                        <button
+                            type="button"
+                            className="btn"
+                            key={category}
+                            onClick={() => filterMenuItems(category, menu)}
+                        >
+                            {category}
+                        </button>
+                    );
+                })}
+        </div>
+    );
+}
+export default CategoryFilter;
